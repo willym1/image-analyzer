c846d377c5b89f7c2b51ae7bfc8dee11d20c7f58
Williandy Mintarja <willym193@gmail.com>
2018-04-22 21:39:22 -0400

    convert tabs to spaces

--- README.md
+++ README.md
@@ -24,38 +24,38 @@ func Run(logging bool) {
 Prints
 ```
 -----apple.jpg-----
-Elapsed: 17.526ms
+Elapsed: 18.0172ms
 Image size: 400 x 456
 Average RGB: 164 80 79
 
 -----gradient.gif-----
-ERROR: - File extension not supported.
+ERROR: File extension not supported.
 
 -----green-apple.jpeg-----
-Elapsed: 23.0251ms
+Elapsed: 22.5211ms
 Image size: 510 x 490
 Average RGB: 146 182 93
 
 -----louise-belcher.png-----
-Elapsed: 33.0317ms
+Elapsed: 23.0281ms
 Image size: 343 x 603
 Average RGB: 175 138 99
 
 -----orange-full.jpg-----
-Elapsed: 49.0464ms
+Elapsed: 48.0457ms
 Image size: 780 x 400
 Average RGB: 242 140 27
 
 -----orange.jpg-----
-Elapsed: 30.0287ms
+Elapsed: 30.5293ms
 Image size: 520 x 520
 Average RGB: 249 148 58
 
 -----invalidimage-----
-ERROR: - open ./gallery/invalidimage: The system cannot find the file specified.
+ERROR: open ./gallery/invalidimage: The system cannot find the file specified.
 
 -----skyline.jpg-----
-Elapsed: 105.1003ms
+Elapsed: 104.6241ms
 Image size: 1300 x 796
 Average RGB: 69 51 64
 ```
--- autoimage/autoimage.go
+++ autoimage/autoimage.go
@@ -1,20 +1,20 @@
 package autoimage
 
 import (
-	"errors"
-	"fmt"
-	"image"
-	"image/jpeg"
-	"image/png"
-	"math"
-	"os"
-	"path/filepath"
-	"time"
+    "errors"
+    "fmt"
+    "image"
+    "image/jpeg"
+    "image/png"
+    "math"
+    "os"
+    "path/filepath"
+    "time"
 )
 
 type Images struct {
-	images []imageData
-	logging bool
+    images []imageData
+    logging bool
 }
 
 /*
@@ -22,100 +22,100 @@ Receives a string of filenames then
 make new image data from each image
 */
 func NewImages(fns []string, l bool) Images {
-	var images []imageData
+    var images []imageData
     for _, v := range fns {
-		// log information if enabled
-		var start time.Time
-		if l {
-			start = time.Now()
-		}
+        // log information if enabled
+        var start time.Time
+        if l {
+            start = time.Now()
+        }
 
-		imgd, err := initImageData(v)
-		if err == nil {
-			images = append(images, imgd)
-			
-			// log information if enabled
-			if l {
-				n := time.Now()
-				s := imgd.Size()
-				rgb := imgd.RGBAvgs()
+        imgd, err := initImageData(v)
+        if err == nil {
+            images = append(images, imgd)
+            
+            // log information if enabled
+            if l {
+                n := time.Now()
+                s := imgd.Size()
+                rgb := imgd.RGBAvgs()
 
-				fmt.Printf("-----%s-----\n", v)
-				fmt.Printf("Elapsed: %s\n", n.Sub(start))
-				fmt.Printf("Image size: %d x %d\n", s.X, s.Y)
-				fmt.Printf("Average RGB: %v %v %v\n", math.Round(float64(rgb[0])), math.Round(float64(rgb[1])), math.Round(float64(rgb[2])))
-				fmt.Println("")
-			}
+                fmt.Printf("-----%s-----\n", v)
+                fmt.Printf("Elapsed: %s\n", n.Sub(start))
+                fmt.Printf("Image size: %d x %d\n", s.X, s.Y)
+                fmt.Printf("Average RGB: %v %v %v\n", math.Round(float64(rgb[0])), math.Round(float64(rgb[1])), math.Round(float64(rgb[2])))
+                fmt.Println("")
+            }
 
-		} else if l {
-			fmt.Printf("-----%s-----\n", v)
-			fmt.Printf("ERROR: - %s\n\n", err)
-		}
-	}
+        } else if l {
+            fmt.Printf("-----%s-----\n", v)
+            fmt.Printf("ERROR: %s\n\n", err)
+        }
+    }
 
-	ai := Images{images: images, logging: l}
-	return ai
+    ai := Images{images: images, logging: l}
+    return ai
 }
 
 
 type imageData interface {
-	RGBAvgs() []float32
-	Size() image.Point
+    RGBAvgs() []float32
+    Size() image.Point
 
-	Scan() [][][]uint8
-	RGBAt(int, int) []uint8
-	CalcAverages() []float32
+    Scan() [][][]uint8
+    RGBAt(int, int) []uint8
+    CalcAverages() []float32
 }
 
 type ImageData struct {
-	image image.Image
-	size image.Point
+    image image.Image
+    size image.Point
 
-	pixels int
-	validPixels int
+    pixels int
+    validPixels int
 
-	colorTable [][][]uint8
-	rgbSums []uint
-	rgbAvgs []float32
+    colorTable [][][]uint8
+    rgbSums []uint
+    rgbAvgs []float32
 }
 
 func initImageData(filename string) (imageData, error) {
-	// looks for the filename in gallery folder
-	reader, err := os.Open(fmt.Sprintf("./gallery/%s", filename))
-	if err != nil {
-		return nil, err
-	}
-	
-	// determine how the file should be decoded from its extension
-	var image image.Image
-	switch filepath.Ext(filename) {
-		case ".jpg", ".jpeg":
-			image, _ = jpeg.Decode(reader)
-		case ".png":
-			image, _ = png.Decode(reader)
-		default:
-			return nil, errors.New("File extension not supported.")
-	}
-	
-	maxBounds := image.Bounds().Max
-	imgd := &ImageData{
-		image: image,
-		size: maxBounds,
-		pixels: maxBounds.X * maxBounds.Y,
-	}
-	
-	imgd.Scan()
-	imgd.CalcAverages()
+    // looks for the filename in gallery folder
+    reader, err := os.Open(fmt.Sprintf("./gallery/%s", filename))
+    if err != nil {
+        return nil, err
+    }
+    
+    // determine how the file should be decoded from its extension
+    var image image.Image
+    switch filepath.Ext(filename) {
+        case ".jpg", ".jpeg":
+            image, _ = jpeg.Decode(reader)
+        case ".png":
+            image, _ = png.Decode(reader)
+        default:
+            return nil, errors.New("File extension not supported.")
+    }
+    
+    maxBounds := image.Bounds().Max
+    imgd := &ImageData{
+        image: image,
+        size: maxBounds,
+        pixels: maxBounds.X * maxBounds.Y,
+    }
+    
+    imgd.Scan()
+    imgd.CalcAverages()
 
-	return imgd, nil
+    return imgd, nil
 }
 
 func (imgd *ImageData) RGBAvgs() []float32 {
-	return imgd.rgbAvgs
+    return imgd.rgbAvgs
 }
 
 func (imgd *ImageData) Size() image.Point {
-	return imgd.size
+    return imgd.size
 }
 
 /*
@@ -124,66 +124,66 @@ Each pixel's RGB values are stored in a 3d array
 and the sums of all RGB colors are collected.
 */
 func (imgd *ImageData) Scan() [][][]uint8 {
-	// reset props
-	imgd.validPixels = 0
-	imgd.rgbSums = make([]uint, 3)
-	imgd.colorTable = make([][][]uint8, imgd.size.Y)
+    // reset props
+    imgd.validPixels = 0
+    imgd.rgbSums = make([]uint, 3)
+    imgd.colorTable = make([][][]uint8, imgd.size.Y)
 
-	for i_y := 0; i_y < imgd.size.Y; i_y++ {
-		imgd.colorTable[i_y] = make([][]uint8, imgd.size.X)
+    for i_y := 0; i_y < imgd.size.Y; i_y++ {
+        imgd.colorTable[i_y] = make([][]uint8, imgd.size.X)
 
-		// assign each nested entry a color
-		for i_x := 0; i_x < imgd.size.X; i_x++ {
-			rgb := imgd.RGBAt(i_x, i_y)[:3]
-			imgd.colorTable[i_y][i_x] = rgb
+        // assign each nested entry a color
+        for i_x := 0; i_x < imgd.size.X; i_x++ {
+            rgb := imgd.RGBAt(i_x, i_y)[:3]
+            imgd.colorTable[i_y][i_x] = rgb
 
-			// append each color to sum
-			if Filter(rgb) {
-				imgd.validPixels++
-				for i, v := range rgb {
-					imgd.rgbSums[i] += uint(v)
-				}
-			}
-		}
-	}
+            // append each color to sum
+            if Filter(rgb) {
+                imgd.validPixels++
+                for i, v := range rgb {
+                    imgd.rgbSums[i] += uint(v)
+                }
+            }
+        }
+    }
 
-	return imgd.colorTable
+    return imgd.colorTable
 }
 
 /*
 Get the uint32 RGB values then convert it to uint8 (0-255)
 */
 func (imgd *ImageData) RGBAt(x, y int) []uint8 {
-	r, g, b, _ := imgd.image.At(x, y).RGBA()
-	return []uint8{
-		uint8(r>>8),
-		uint8(g>>8),
-		uint8(b>>8),
-	}
+    r, g, b, _ := imgd.image.At(x, y).RGBA()
+    return []uint8{
+        uint8(r>>8),
+        uint8(g>>8),
+        uint8(b>>8),
+    }
 }
 
 /*
 Calculate the average RGB values from the total of valid pixels
 */
 func (imgd *ImageData) CalcAverages() []float32 {
-	// reset averages
-	imgd.rgbAvgs = make([]float32, 3)
+    // reset averages
+    imgd.rgbAvgs = make([]float32, 3)
 
-	for i, v := range imgd.rgbSums {
-		imgd.rgbAvgs[i] = float32(v) / float32(imgd.validPixels)
-	}
-	
-	return imgd.rgbAvgs
+    for i, v := range imgd.rgbSums {
+        imgd.rgbAvgs[i] = float32(v) / float32(imgd.validPixels)
+    }
+    
+    return imgd.rgbAvgs
 }
 
 /*
 Determine which colors can be processed
 */
 func Filter(rgb []uint8) bool {
-	// skip if pixel is white or black
-	if (rgb[0] == 255 && rgb[1] == 255 && rgb[2] == 255) || (rgb[0] == 0 && rgb[1] == 0 && rgb[2] == 0) {
-		return false
-	}
+    // skip if pixel is white or black
+    if (rgb[0] == 255 && rgb[1] == 255 && rgb[2] == 255) || (rgb[0] == 0 && rgb[1] == 0 && rgb[2] == 0) {
+        return false
+    }
 
-	return true
+    return true
 }
